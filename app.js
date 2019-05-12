92147c2accc9858ac57f4599c78278bbcd87384e
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2019-05-12 15:07:14 +0100

    mvp of backend api

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const router = require('./router/api');
 
 app.use('/api', router);
 
+//error handler
+app.use((err, req, res, next) => {
+  res.send({ error: err });
+});
+
 app.listen(process.env.port || 9090, () => {
   console.log('app listening');
 });
--- router/api.js
+++ router/api.js
@@ -4,5 +4,3 @@ const { getData } = require('../controllers/getData');
 router.use('/destination', getData);
 
 module.exports = router;
-
-// https://maps.googleapis.com/maps/api/distancematrix/json?origins=Manchester,UK&destinations=London,UK&key=AIzaSyD4reoMLRkzR9Htv4PmIuzAV99pP-P2Lqs
