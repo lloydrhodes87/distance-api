fc80f8b3eeb3ab36986c856bac3b81076cb863ff
Lloyd Rhodes <lloydrhodes@hotmail.co.uk>
2019-05-12 16:33:14 +0100

    enable cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 require('dotenv').config();
+app.use(cors());
 
 const router = require('./router/api');
 
